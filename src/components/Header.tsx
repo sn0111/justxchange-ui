7982d50c12cb0aa0fec68aa2691e1e0d0238b805
vinod kumar <vinnuvinodkumar8080@gmail.com>
2025-01-01 21:42:29 +0530

    mobile menu issue fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import {
   FaBars,
   FaTimes,
@@ -26,6 +26,8 @@ const SearchButton = () => (
   </Link>
 );
 export const Header = () => {
+  const nodeRef = useRef(null);
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { authenticationToken, logout } = useAuth();
   const currentPath = usePathname();
@@ -59,26 +61,26 @@ export const Header = () => {
         {authenticationToken && (
           <>
             <div className="hidden md:flex items-center gap-3">
-  <Link href="/add-product">
-    <button className="flex items-center gap-2 px-4 py-2 bg-blue-500 text-white text-sm font-medium rounded-lg shadow-md hover:bg-blue-700 focus:outline-none">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-5 h-5"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M12 4.5v15m7.5-7.5h-15"
-        />
-      </svg>
-      <span>Sell</span>
-    </button>
-  </Link>
-</div>
+              <Link href="/add-product">
+                <button className="flex items-center gap-2 px-4 py-2 bg-blue-500 text-white text-sm font-medium rounded-lg shadow-md hover:bg-blue-700 focus:outline-none">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 4.5v15m7.5-7.5h-15"
+                    />
+                  </svg>
+                  <span>Sell</span>
+                </button>
+              </Link>
+            </div>
 
             {/* Conditionally hide SearchButton */}
             {!currentPath.includes('/search') && (
@@ -118,6 +120,7 @@ export const Header = () => {
         timeout={300}
         classNames="menu"
         unmountOnExit
+        nodeRef={nodeRef}
       >
         <div className="fixed top-0 left-0 w-3/4 h-full bg-white shadow-lg z-50 p-4 overflow-y-auto transition-transform transform translate-x-0">
           <div className="flex items-center justify-between mb-4">
